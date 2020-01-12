1054de61084607ec1ac55cfb1787bec8b57c4534
l1z1b <echoi@alumni.ubc.ca>
2020-01-11 22:00:17 -0800

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyCmdC4jEXXVKz1yJwxm1AxQOpKkmY4G0fI",
+  authDomain: "salestalk-8b506.firebaseapp.com",
+  databaseURL: "https://salestalk-8b506.firebaseio.com",
+  projectId: "salestalk-8b506",
+  storageBucket: "salestalk-8b506.appspot.com",
+  messagingSenderId: "1084331870891",
+  appId: "1:1084331870891:web:f651f279f41e7ee4a1c5ee",
+  measurementId: "G-B5JHS78X2Q"
+};
+firebase.initializeApp(config);
+export default firebase;
