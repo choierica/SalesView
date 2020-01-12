b62539dbd337bd3571051b92b2d570d2898e40b3
hyesunan <hyesun@alumni.ubc.ca>
2020-01-12 10:32:19 -0800

    fixed center bug

--- src/assets/jss/material-dashboard-react/layouts/adminStyle.js
+++ src/assets/jss/material-dashboard-react/layouts/adminStyle.js
@@ -8,8 +8,7 @@ const appStyle = theme => ({
   wrapper: {
     position: "relative",
     top: "0",
-    height: "100vh",
-    textAlign: "center"
+    height: "100vh"
   },
   mainPanel: {
     [theme.breakpoints.up("md")]: {
